3e3f5db222c2f034d3d286ea05a95d75ef134447
Paul K <59290939+paulkakell@users.noreply.github.com>
2025-06-08 23:48:54 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, request, redirect, url_for
 from . import app, db
-from .models import Timer
+from .models import Timer, User
 from flask_login import current_user
 
 @app.route('/')
 def index():
-    return render_template('index.html', timers=Timer.query.all())
+    timers = Timer.query.all()
+    return render_template('index.html', timers=timers)
+
+@app.route('/admin')
+def admin_dashboard():
+    users = User.query.all()
+    return render_template('admin_dashboard.html', users=users)
+
+@app.route('/timer/<int:timer_id>')
+def view_timer(timer_id):
+    timer = Timer.query.get(timer_id)
+    if timer:
+        return render_template('view_timer.html', timer=timer)
+    return redirect(url_for('index'))
